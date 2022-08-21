c690de9a9f43144f0f6fc72c24a0e8254a66d61d
Bryhilevych Orest <91126054+OrestBruhylevych@users.noreply.github.com>
2022-08-21 09:36:24 +0300

    add avto prettier 2

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,46 +1,39 @@
-import { Box } from "components/Box/Box";
-import ChatsList from "components/ChatsList/ChatsList";
-import { SearchBox } from "components/SearchBox/SearchBox";
-import User from "components/User/User";
-import { useState } from "react";
+import { Box } from 'components/Box/Box';
+import ChatsList from 'components/ChatsList/ChatsList';
+import { SearchBox } from 'components/SearchBox/SearchBox';
+import User from 'components/User/User';
+import { useState } from 'react';
 
+export default function AppBar({ chats }) {
+  const [filter, setFilter] = useState('');
 
-
-
-export default function AppBar({chats}) {
-
-    const [filter, setFilter] = useState('');
-
-    const getVisibleChats = () => {
-        const normalizedFilter = filter.toLowerCase().trim();
-        const visibleChats = chats.filter(chat =>
-            chat.name.toLowerCase().includes(normalizedFilter)
-        );
-   
+  const getVisibleChats = () => {
+    const normalizedFilter = filter.toLowerCase().trim();
+    const visibleChats = chats.filter(chat =>
+      chat.name.toLowerCase().includes(normalizedFilter)
+    );
 
     return visibleChats;
-    };
-
-
-     const changeFilter = e => {
-         setFilter(e.currentTarget.value);     
   };
-    
-    return (
-        <Box as="header"   border="1px solid black">
-            <Box bg='grey' p={4} >
-                <User photoUrl={'https://assets.stickpng.com/images/585e4bcdcb11b227491c3396.png'} />
-                <SearchBox value={filter} onChange={changeFilter} />
-            </Box>
-            <Box p={4}>
-                <h2>Chats</h2>
-                <ChatsList chats={getVisibleChats()} />
-            </Box>
-            
-        </Box>
-        
-    );
-  
-    
 
-};
+  const changeFilter = e => {
+    setFilter(e.currentTarget.value);
+  };
+
+  return (
+    <Box as="header" border="1px solid black">
+      <Box bg="grey" p={4}>
+        <User
+          photoUrl={
+            'https://assets.stickpng.com/images/585e4bcdcb11b227491c3396.png'
+          }
+        />
+        <SearchBox value={filter} onChange={changeFilter} />
+      </Box>
+      <Box p={4}>
+        <h2>Chats</h2>
+        <ChatsList chats={getVisibleChats()} />
+      </Box>
+    </Box>
+  );
+}
